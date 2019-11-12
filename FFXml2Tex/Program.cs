f6969a7fa21ed7108e87dd840eb2953c1eccff4a
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-13 00:50:16 +0900

    Project Setup

---
+++ FFXml2Tex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FFXml2Tex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
