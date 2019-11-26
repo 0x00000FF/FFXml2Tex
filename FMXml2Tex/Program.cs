8abb6423e33e4eb9daebfc2b717605251495a97f
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-26 16:45:36 +0900

    Branding, File Opening with Arguments

--- FMXml2Tex/Program.cs
+++ FMXml2Tex/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FMXml2Tex
 {
@@ -6,7 +7,28 @@ namespace FMXml2Tex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(
+                $"FrameMaker XML to LaTeX Transpiler {Environment.Version.ToString()}\n" +
+                $"Copyright (C) 0x00000FF, {DateTime.Now.Year} All rights reserved.\n"
+                );
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: Target file path is not provided.");
+                Console.WriteLine("Usage: dotnet FMXml2Tex.dll <framemaker-xml-file>");
+                
+                Environment.Exit(1);
+            }
+
+            var filepath = args[0];
+            
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine("Error: Target file path does not exists!");
+                Environment.Exit(2);
+            }
+
+
         }
     }
 }
